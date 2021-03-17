77e9160da9b93da4f107f70540491dede196d16d
wndeld777 <wndeld777@gmail.com>
2021-03-17 11:50:40 +0900

    Valuse

---
+++ Java_001_Value/src/com/callor/values/Values_02.java
@@ -0,0 +1,16 @@
+package com.callor.values;
+
+public class Values_02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("===================");
+		System.out.println("반지름이 23인 원의 둘레");
+		System.out.println("23 * 2 * 3.14 = " + (23 * 2 * 3.14));
+		System.out.println("===================");
+		System.out.println("반지름이 23인 원의 넓이");
+		System.out.println("23 * 23 * 3.14 = " + (23 * 23 * 3.14));
+		System.out.println("==================="); 
+	
+	}
+}
